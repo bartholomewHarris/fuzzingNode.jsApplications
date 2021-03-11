9d533d70e77be6db34de81bae27d29d50ba94ae7
williamboston <williamboston1@gmail.com>
2021-03-12 10:39:55 +1100

    small updates to attack script + local report folder added

--- Juice-Shop/juice-shop/3_attack.py
+++ Juice-Shop/juice-shop/3_attack.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import subprocess
 import os
@@ -8,12 +8,13 @@ os.chdir("/snap/zaproxy/5")
 
 #run basic scan on juiceshop through zap
 subprocess.call('zap-cli quick-scan --spider http://localhost:3000', shell=True)
+#subprocess.call('zap-cli quick-scan --ajax-spider http://localhost:3000', shell=True)
 
 #change directory to where report should be saved
-os.chdir("/home/william/Documents")
+os.chdir("/home/william/Documents/Uni/fuzzingNode.jsApplications/reports")
 
 #export results as html file
 subprocess.call('zap-cli report -o zap_report_juiceshop.html -f html', shell=True)
 
 #persist the window to view results
-raw_input("\n Press enter to close window...")
+input("\n Press enter to close window...")
