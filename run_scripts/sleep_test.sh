522bf04b4a0b47d487e9bdb83d6b395144cbe7ce
Bart <bartholomew.harris@connect.qut.edu.au>
2021-03-19 13:21:49 +1000

    sleep_test.sh

---
+++ run_scripts/sleep_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Name: sleep_test.sh
+# ----------------------------------------
+SLEEP_TIME="20"
+echo "Sleeping for ${SLEEP_TIME}"
+sleep ${SLEEP_TIME}
+echo "Sleep time has elapsed"
