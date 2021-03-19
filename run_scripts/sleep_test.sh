d72ac8cbddbb422e6b21c41f26d2186ac5bb141f
Bart <bartholomew.harris@connect.qut.edu.au>
2021-03-19 13:43:08 +1000

    Fixed wait for ZAP to boot

--- run_scripts/sleep_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Name: sleep_test.sh
-# ----------------------------------------
-SLEEP_TIME="20"
-echo "Sleeping for ${SLEEP_TIME}"
-sleep ${SLEEP_TIME}
-echo "Sleep time has elapsed"
