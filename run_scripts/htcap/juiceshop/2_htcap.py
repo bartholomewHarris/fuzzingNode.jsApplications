23db67a3a1921d00240a851ade5e3c50bee84f2f
williamboston <williamboston1@gmail.com>
2021-03-26 12:21:19 +1100

    Update 2_htcap.py

--- run_scripts/htcap/juiceshop/2_htcap.py
+++ run_scripts/htcap/juiceshop/2_htcap.py
@@ -13,7 +13,7 @@ os.chdir('./../../../htcap')
 #cmd = "./htcap.py crawl -w -t %d -x http://localhost:3000/redirect. http://localhost:3000 htcap_juiceshop_report.db \; scan native \; scan arachni \; scan sqlmap \; scan wapiti \; util report htcap_juiceshop_report.html" % SPIDERTIME
 
 #crawl & scan & save report - native+arachni+sqlmap
-subprocess.call("./htcap.py crawl -w -t 1800 -x http://localhost:3000/redirect. http://localhost:3000 htcap_juiceshop_report.db \; scan native \; scan arachni \; scan sqlmap \; scan wapiti \; util report htcap_juiceshop_report.html", shell=True)
+subprocess.call("./htcap.py crawl -w -t 60 -x http://localhost:3000/redirect. http://localhost:3000 htcap_juiceshop_report.db \; scan native \; scan arachni \; scan sqlmap \; scan wapiti \; util report htcap_juiceshop_report.html", shell=True)
 
 #move the report to the report folder
 subprocess.call('mv htcap_juiceshop_report.html ./../reports', shell=True)
