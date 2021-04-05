0adf55204a36e10193775952bc3fc68e5af13ab6
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-04-05 18:58:32 +1000

    changed dir for arachni_attack.py before executing

--- run_scripts/arachni/juiceshop/arachni_attack.py
+++ run_scripts/arachni/juiceshop/arachni_attack.py
@@ -3,5 +3,7 @@
 import subprocess
 import os
 
-subprocess.call('./arachni-1.5.1-0.5.12/bin/arachni --output-verbose --output-only-positives --scope-include-subdomains --scope-exclude-pattern http://127.0.0.1:3000/redirect \
+os.chdir("~/fuzzingNode.jsApplications/arachni-1.5.1-0.5.12/bin")
+
+subprocess.call('./arachni --output-verbose --output-only-positives --scope-include-subdomains --scope-exclude-pattern http://127.0.0.1:3000/redirect \
     --report-save-path /reports/arachni_juiceshop_report http://127.0.0.1:3000', shell=True)
