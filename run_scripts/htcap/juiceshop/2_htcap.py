f12c7c87725019f276f904bdd1fe290ca135886b
williamboston <williamboston1@gmail.com>
2021-04-07 14:18:03 +1000

    adding jak for reference - will not work

--- run_scripts/htcap/juiceshop/2_htcap.py
+++ run_scripts/htcap/juiceshop/2_htcap.py
@@ -11,7 +11,6 @@ SPIDERTIME = 20
 
 #form the command
 cmd = "./htcap.py crawl -w -t %d -x http://localhost:3000/redirect. http://localhost:3000 htcap_juiceshop_report.db \; scan native \; scan arachni \; scan sqlmap \; scan wapiti \; util report htcap_juiceshop_report.html" % SPIDERTIME
-#cmd = "./htcap.py crawl -w -t %d -x http://localhost:3000/redirect. http://localhost:3000 htcap_juiceshop_report.db \; util report htcap_juiceshop_report.html" % SPIDERTIME
 
 #crawl & scan & save report - native+arachni+sqlmap+wapiti
 subprocess.call(cmd, shell=True)
@@ -20,4 +19,4 @@ subprocess.call(cmd, shell=True)
 subprocess.call('mv htcap_juiceshop_report.html ./../reports', shell=True)
 
 #persist the window to view results
-#input("\n Press enter to close window...")
+input("\n Press enter to close window...")
