5ba19a05816d6dbdf959e8113adbf3db89d7731b
williamboston <williamboston1@gmail.com>
2021-04-09 10:32:50 +1000

    wapiti report working locally

--- run_scripts/wapiti/juiceshop/2_wapiti.py
+++ run_scripts/wapiti/juiceshop/2_wapiti.py
@@ -3,13 +3,19 @@
 import subprocess
 import os
 
+subprocess.call("sudo apt install rename", shell=True)
+
+os.chdir("../../../")
+
 #form the command
-cmd = "wapiti -u http://localhost:3000"
+cmd = "wapiti -u http://localhost:3000 --scope domain -o ./reports/wapiti"
 
 #crawl & scan & save report - native+sqlmap+wapiti
 subprocess.call(cmd, shell=True)
 
-#move the report to the report folder
+#re-name report
+os.chdir("./reports/wapiti")
+subprocess.call("rename 's/localhost_3000_.*.html/wapiti_juiceshop_report.html/' *", shell=True)
 
 #persist the window to view results
 #input("\n Press enter to close window...")
