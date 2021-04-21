a85a6406c80bfc3b89e39253426272740884f04d
williamboston <williamboston1@gmail.com>
2021-04-21 15:49:42 +1000

    Update 3_attack.py

--- run_scripts/zap/nodegoat/3_attack.py
+++ run_scripts/zap/nodegoat/3_attack.py
@@ -20,7 +20,7 @@ start1 = time.time()
 #crawl
 subprocess.call('zap-cli open-url http://localhost:4000', shell=True)
 subprocess.call('zap-cli -v spider -c NodeGoat-Auth -u admin http://localhost:4000', shell=True)
-subprocess.call('zap-cli -v ajax-spider http://localhost:4000/benefits', shell=True)
+subprocess.call('zap-cli -v ajax-spider http://localhost:4000', shell=True)
 
 #calculate scan time
 end1 = time.time()
