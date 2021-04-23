e009a805bedc424c4a0ea9e259d98fe29f62b960
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-04-23 11:23:02 +1000

    arachni v1.3

--- run_scripts/arachni/juiceshop/arachni_attack.py
+++ run_scripts/arachni/juiceshop/arachni_attack.py
@@ -3,7 +3,7 @@
 import subprocess
 import os
 
-os.chdir("./../../../arachni-1.5.1-0.5.12/bin")
+os.chdir("./../../../arachni-1.3-0.5.8/bin")
 
 subprocess.call('./arachni --output-verbose --output-only-positives --scope-include-subdomains --scope-exclude-pattern http://127.0.0.1:3000/redirect \
     --report-save-path /reports/arachni_juiceshop_report <HOSTNAME>', shell=True)
