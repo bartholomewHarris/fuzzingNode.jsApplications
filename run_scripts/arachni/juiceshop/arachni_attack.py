f0538c92a15b359d6e7ea6c3e5517687867697a2
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-04-26 14:57:59 +1000

    decode hostname to str

--- run_scripts/arachni/juiceshop/arachni_attack.py
+++ run_scripts/arachni/juiceshop/arachni_attack.py
@@ -6,6 +6,7 @@ import os
 os.chdir("./../../../arachni-1.5.1-0.5.12/bin")
 
 hostname = subprocess.check_output(["hostname"])
+hostname = hostname.decode("utf-8")
 
 target = "http://" + hostname + ":3000"
 
