92eb8c351b454ed5c1bee94fffb1d2a596bfaa62
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-04-26 15:11:33 +1000

    arachni try simple scan

--- run_scripts/arachni/juiceshop/arachni_attack.py
+++ run_scripts/arachni/juiceshop/arachni_attack.py
@@ -15,4 +15,6 @@ exclude = target + "/redirect"
 command = "'./arachni --output-verbose --output-only-positives --scope-include-subdomains --scope-exclude-pattern "
 command = command + exclude + " --report-save-path /reports/arachni_juiceshop_report " + target + "'"
 
-subprocess.call(command, shell=True)
+simple_scan = "./arachni " + target
+
+subprocess.call(simple_scan, shell=True)
