2b49049cecfa4471c5bc232eafc82f74bed7e13d
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-04-26 14:51:16 +1000

    receive string from subprocess.check_output

--- run_scripts/arachni/juiceshop/arachni_attack.py
+++ run_scripts/arachni/juiceshop/arachni_attack.py
@@ -5,7 +5,7 @@ import os
 
 os.chdir("./../../../arachni-1.5.1-0.5.12/bin")
 
-hostname = subprocess.check_output('hostname')
+hostname = subprocess.check_output(["hostname"])
 
 target = "http://" + hostname + ":3000"
 
