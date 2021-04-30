4a2983e526a3ebe80a7d971ef410d8528c6764c7
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-04-30 21:19:28 +1000

    arachni fix report export

--- run_scripts/arachni/juiceshop/arachni_attack.py
+++ run_scripts/arachni/juiceshop/arachni_attack.py
@@ -15,7 +15,7 @@ target = target + ":3000"
 exclude = target + "/redirect"
 
 command = "./arachni --output-verbose --output-only-positives --scope-include-subdomains --scope-exclude-pattern "
-command = command + exclude + " --report-save-path /reports/arachni_juiceshop_report.afr --timeout 0:3:0 "
+command = command + exclude + " --report-save-path arachni_juiceshop_report.afr --timeout 0:3:0 "
 command = command + "--http-authentication-username admin@juice-sh.op --http-authentication-password admin123 " + target
 
 subprocess.call(command, shell=True)
