465814ff4ee99942210b2f2ed82b5852b1254993
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-04-30 13:45:19 +1000

    arachni_attack fixed command call

--- run_scripts/arachni/juiceshop/arachni_attack.py
+++ run_scripts/arachni/juiceshop/arachni_attack.py
@@ -16,6 +16,6 @@ exclude = target + "/redirect"
 
 command = "./arachni --output-verbose --output-only-positives --scope-include-subdomains --scope-exclude-pattern "
 command = command + exclude + " --report-save-path /reports/arachni_juiceshop_report --timeout 5:50:00 "
-command = command + "--http-authentication-username admin@juice-sh.op --http-authentication-password admin123" + target
+command = command + "--http-authentication-username admin@juice-sh.op --http-authentication-password admin123 " + target
 
 subprocess.call(command, shell=True)
