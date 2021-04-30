f09d1ba05c5d8abada90f99b3b8bfee13d6dadad
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-04-30 13:36:01 +1000

    arachni added auth and timeout

--- run_scripts/arachni/juiceshop/arachni_attack.py
+++ run_scripts/arachni/juiceshop/arachni_attack.py
@@ -15,8 +15,7 @@ target = target + ":3000"
 exclude = target + "/redirect"
 
 command = "./arachni --output-verbose --output-only-positives --scope-include-subdomains --scope-exclude-pattern "
-command = command + exclude + " --report-save-path /reports/arachni_juiceshop_report " + target
-
-simple_scan = "./arachni " + target
+command = command + exclude + " --report-save-path /reports/arachni_juiceshop_report --timeout 5:50:00 "
+command = command + "--http-authentication-username admin@juice-sh.op --http-authentication-password admin123" + target
 
 subprocess.call(command, shell=True)
