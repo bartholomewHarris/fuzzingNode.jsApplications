ba747304f268f9ce70b5d3e1ec5e13e45efce6f1
williamboston <williamboston1@gmail.com>
2021-04-30 13:54:56 +1000

    Update detacher.py

--- run_scripts/zap/mongo-express/detacher.py
+++ run_scripts/zap/mongo-express/detacher.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
 import os
+import subprocess
+
+subprocess.call('sudo apt update', shell=True)
+subprocess.call('sudo apt install gnome-terminal', shell=True)
 
 os.system("gnome-terminal -- python express-launcher.py")
