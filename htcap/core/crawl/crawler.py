36f6d00c5771d4332419484b7485af98111c30b4
williamboston <williamboston1@gmail.com>
2021-05-02 13:23:38 +1000

    Update crawler.py

--- htcap/core/crawl/crawler.py
+++ htcap/core/crawl/crawler.py
@@ -607,8 +607,8 @@ class Crawler:
 		Shared.probe_cmd = probe_cmd + probe_options
 
 
-		Shared.starturl = normalize_url(args[0])
-		out_file = args[1]
+		# Shared.starturl = normalize_url(args[0])
+		# out_file = args[1]
 
 		purl = urlsplit(Shared.starturl)
 		Shared.allowed_domains.add(purl.hostname)
