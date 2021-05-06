bffb7d1c94e702a21e295d882d68b28831902bd1
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-05-07 07:42:13 +1000

    mongo_arachni

---
+++ run_scripts/arachni/mongo-express/arachni_attack.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import subprocess
+import os
+
+os.chdir("./../../../arachni-1.5.1-0.5.12/bin")
+
+hostname = subprocess.check_output(["hostname"])
+hostname = hostname.decode("utf-8")
+
+target = "admin:pass@" + hostname 
+target = target.rstrip("\n")
+target = target + ":8081"
+
+exclude = target + "/redirect"
+
+command = "./arachni --output-verbose --output-only-positives --scope-include-subdomains "
+command = command + "--report-save-path arachni_report.afr --timeout 0:3:0 "
+command = command + "--http-authentication-username admin --http-authentication-password pass " + target
+
+subprocess.call(command, shell=True)
+
+subprocess.call("./arachni_reporter arachni_report.afr --reporter=html:outfile=arachni_report.html.zip", shell=True)
