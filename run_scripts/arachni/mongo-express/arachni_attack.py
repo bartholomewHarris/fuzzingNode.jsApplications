541d1c80767e92b5b43007e0a6616d7a02081ff4
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-05-07 08:33:22 +1000

    arachni_mongo changed host

--- run_scripts/arachni/mongo-express/arachni_attack.py
+++ run_scripts/arachni/mongo-express/arachni_attack.py
@@ -5,15 +5,14 @@ import os
 
 os.chdir("./../../../arachni-1.5.1-0.5.12/bin")
 
-# hostname = subprocess.check_output(["hostname"])
-# hostname = hostname.decode("utf-8")
-hostname = "138.0.0.1"
+hostname = subprocess.check_output(["hostname"])
+hostname = hostname.decode("utf-8")
 
 target = "http://admin:pass@" + hostname 
 target = target.rstrip("\n")
 target = target + ":8081"
 
-exclude = target + "/redirect"
+# exclude = target + "/redirect"
 
 command = "./arachni --output-verbose --output-only-positives --scope-include-subdomains "
 command = command + "--report-save-path arachni_report.afr --timeout 0:3:0 "
