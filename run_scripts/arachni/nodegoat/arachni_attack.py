81d038f862321c87a0781d62b94726c628103456
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-05-07 10:46:31 +1000

    arachni_nodegoat

--- run_scripts/arachni/nodegoat/arachni_attack.py
+++ run_scripts/arachni/nodegoat/arachni_attack.py
@@ -8,16 +8,15 @@ os.chdir("./../../../arachni-1.5.1-0.5.12/bin")
 hostname = subprocess.check_output(["hostname"])
 hostname = hostname.decode("utf-8")
 
-# try taking out admin:pass@ before hostname
 target = "http://" + hostname 
 target = target.rstrip("\n")
-target = target + ":8081"
+target = target + ":4000"
 
 # exclude = target + "/redirect"
 
-command = "./arachni --output-verbose --output-only-positives --scope-include-subdomains "
-command = command + "--report-save-path arachni_report.afr --timeout 0:3:0 "
-command = command + "--http-authentication-username admin --http-authentication-password pass " + target
+command = "./arachni --output-verbose --output-only-positives --scope-include-subdomains --scope-exclude-pattern "
+command = command + " --report-save-path arachni_report.afr --timeout 0:3:0 "
+command = command + "--http-authentication-username admin --http-authentication-password Admin_123 " + target
 
 subprocess.call(command, shell=True)
 
