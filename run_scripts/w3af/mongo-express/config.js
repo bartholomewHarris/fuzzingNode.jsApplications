7c4c29cd7ab104d911309e889d8b573457fdef00
bartholomewHarris <bartholomew.harris@connect.qut.edu.au>
2021-05-21 10:29:14 +1000

    update mongo_w3af_config.js

--- run_scripts/w3af/mongo-express/config.js
+++ run_scripts/w3af/mongo-express/config.js
@@ -137,12 +137,12 @@ module.exports = {
   // set useBasicAuth to true if you want to authenticate mongo-express logins
   // if admin is false, the basicAuthInfo list below will be ignored
   // this will be true unless ME_CONFIG_BASICAUTH_USERNAME is set and is the empty string
-  //useBasicAuth: getFileEnv(basicAuthUsername) !== '',
+  useBasicAuth: getFileEnv(basicAuthUsername) !== '',
 
-  //basicAuth: {
-  //  username: getFileEnv(basicAuthUsername) || '',
-  //  password: getFileEnv(basicAuthPassword) || '',
-  //},
+  basicAuth: {
+    username: getFileEnv(basicAuthUsername) || '',
+    password: getFileEnv(basicAuthPassword) || '',
+  },
   
   
 
